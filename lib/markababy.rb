e0d29826514ef00fb3e6b0da55da70614db3d4c3
Tim Craft <mail@timcraft.com>
2011-10-10 20:36:11 +0100

    Add functionality for rendering tags without attributes or inner content

--- lib/markababy.rb
+++ lib/markababy.rb
@@ -1,2 +1,17 @@
 module Markababy
+  def self.capture(&block)
+    [].tap { |output| Builder.new(output, &block) }.join
+  end
+
+  class Builder < BasicObject
+    def initialize(output, &block)
+      @output = output
+
+      instance_eval(&block)
+    end
+
+    def method_missing(sym, *args, &block)
+      @output << "<#{sym}>"
+    end
+  end
 end
--- spec/markababy_spec.rb
+++ spec/markababy_spec.rb
@@ -2,4 +2,7 @@ require 'minitest/autorun'
 require 'markababy'
 
 describe Markababy do
+  it 'should render a tag without attributes or content correctly' do
+    Markababy.capture { br }.must_equal '<br>'
+  end
 end
